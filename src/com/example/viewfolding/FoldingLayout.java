af44f81fefbe8e08a46001c465615ec57faeb722
rocel <damien.chaillou@gmail.com>
2013-02-10 23:44:16 +0100

    before COUNTER_CLOCK changes

--- src/com/example/viewfolding/FoldingLayout.java
+++ src/com/example/viewfolding/FoldingLayout.java
@@ -17,9 +17,9 @@ import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.FrameLayout;
 
 public class FoldingLayout extends FrameLayout {
-	private static final int MODE_TO_RIGHT = 0;
-	private static final int MODE_TO_LEFT = 1;
-	private static final int FOLDS_SIZE = 50;
+	private static final int MODE_TO_COUNTER_CLOCK = 0;
+	private static final int MODE_TO_CLOCK = 1;
+	private static final int FOLDS_SIZE = 100;
 	
 	private int nbFolds = 2;
 	
@@ -49,10 +49,8 @@ public class FoldingLayout extends FrameLayout {
 				nbFolds = mBitmap.getWidth()/FOLDS_SIZE;
 				Log.d(this.getClass().getName(),"nbFolds = " + nbFolds);
 				for(int i = 0; i < nbFolds ; i++){
-//					Bitmap.createBitmap(bitmap, 	0, 					 0, bitmap.getWidth()/2, bitmap.getHeight()); //LEFT
-//					Bitmap.createBitmap(bitmap, 	bitmap.getWidth()/2, 0, bitmap.getWidth()/2, bitmap.getHeight()); //RIGHT
-					Log.d(this.getClass().getName(), i + " -> x : " + (i==0?0:mBitmap.getWidth()/nbFolds*i));
-					Log.d(this.getClass().getName(), i + " -> width : " + (mBitmap.getWidth()/nbFolds));
+//					Log.d(this.getClass().getName(), i + " -> x : " + (i==0?0:mBitmap.getWidth()/nbFolds*i));
+//					Log.d(this.getClass().getName(), i + " -> width : " + (mBitmap.getWidth()/nbFolds));
 					
 					Bitmap nbitmap = Bitmap.createBitmap(mBitmap,
 							(i==0?0:mBitmap.getWidth()/nbFolds*i),
@@ -60,7 +58,7 @@ public class FoldingLayout extends FrameLayout {
 							mBitmap.getWidth()/nbFolds,
 							mBitmap.getHeight());
 					if(i%2==0){
-						mBitmaps.add(doBrightness(nbitmap,-50));
+						mBitmaps.add(doBrightness(nbitmap,-90));
 					} else {
 						mBitmaps.add(nbitmap);
 					}
@@ -117,6 +115,7 @@ public class FoldingLayout extends FrameLayout {
 		// return final image
 		return bmOut;
 	}
+	
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		Matrix m = new Matrix();
@@ -124,7 +123,11 @@ public class FoldingLayout extends FrameLayout {
 			System.out.println("-------------------- DRAWING !!!!");
 			getChildAt(0).setVisibility(View.GONE);
 			for( int i = 0 ; i<nbFolds ;i++ ){
-		        canvas.drawBitmap(mBitmaps.get(i),getMatrix(0, MODE_TO_LEFT,i), null);
+				if(i%2==0){
+					canvas.drawBitmap(mBitmaps.get(i),getMatrix(0, MODE_TO_CLOCK,i), null);
+				} else {
+					canvas.drawBitmap(mBitmaps.get(i),getMatrix(0, MODE_TO_COUNTER_CLOCK,i), null);
+				}
 			}
 		} else {
 			for (int i = 0; i < getChildCount(); i++) {
@@ -133,36 +136,39 @@ public class FoldingLayout extends FrameLayout {
 		}
 	}
 	
-//	@Override
-//	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-//		if(widthMeasureSpec>0){
-//			mWidthMeasureSpec = widthMeasureSpec;
-//		}
-//		if(heightMeasureSpec>0){
-//			mHeightMeasureSpec = heightMeasureSpec;
-//		}
-//		if(mWidthMeasureSpec>0 && mHeightMeasureSpec>0){
-//			this.setMeasuredDimension(mWidthMeasureSpec, mHeightMeasureSpec);
-//		}
-//	}
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		if(widthMeasureSpec>0){
+			mWidthMeasureSpec = View.MeasureSpec.getSize(widthMeasureSpec);
+		}
+		if(heightMeasureSpec>0){
+			mHeightMeasureSpec = View.MeasureSpec.getSize(heightMeasureSpec);
+		}
+
+		Log.d(this.getClass().getName(), "widthMeasureSpec : "+View.MeasureSpec.getSize(widthMeasureSpec));
+		Log.d(this.getClass().getName(), "heightMeasureSpec : "+View.MeasureSpec.getSize(heightMeasureSpec));
+		
+		if(folding) {
+			//TODO : once it is folded in , reMeasure with smaller size accoring to size.
+			this.setMeasuredDimension(mBitmap.getWidth(), mBitmap.getHeight());
+		}
+	}
 
 	private Matrix getMatrix(final float centerY, final int mode,final int position) {
-		Camera camera = new Camera();
+        Camera camera = new Camera();
 		final Matrix matrix = new Matrix();
         camera.save();
-        if (mode == MODE_TO_RIGHT) {
-        	camera.rotateY(135);
-		} else if (mode == MODE_TO_LEFT) {
+        if (mode == MODE_TO_COUNTER_CLOCK) {
+        	camera.rotateY(-45);
+		} else if (mode == MODE_TO_CLOCK) {
 			camera.rotateY(45);
 		}
         camera.getMatrix(matrix);
         camera.restore();
-        matrix.preTranslate(0, -centerY/2);
+        matrix.preTranslate(0, -centerY/2);;
         matrix.postTranslate(position==0?0:mBitmap.getWidth()/nbFolds*position, 0);
-//        if (mode == MODE_TO_RIGHT) {
-//        	matrix.postScale(-1.0f, 1.0f);
-//        }
+        
 		return matrix;
 	}
 }
